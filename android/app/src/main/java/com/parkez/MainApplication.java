881816a87f8ddd16fe79ec8434c5f370da4e1f1c
subtleseeker <nikhilmundra29@gmail.com>
2018-10-20 03:00:19 +0530

    Before opening studio

--- android/app/src/main/java/com/parkez/MainApplication.java
+++ android/app/src/main/java/com/parkez/MainApplication.java
@@ -26,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new ReactMaterialKitPackage(),
-            new RNGoogleSigninPackage()
+            new RNGoogleSigninPackage(),
+            new GooglePackage()
       );
     }
 
